42c7485b07abf32be6b9091859f5ca278b8a400b
Enzo-Leal <enzo.leal.09@gmail.com>
2023-11-17 10:12:54 -0300

    django rest framework basic implementation

--- Sambook/settings.py
+++ Sambook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core', # new
+    'rest_framework', # new
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,13 @@ TEMPLATES = [
         },
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 WSGI_APPLICATION = 'Sambook.wsgi.application'
 
