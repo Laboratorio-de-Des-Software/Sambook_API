c6c35006d4ad2059183212f5f447d618f165e92e
Gabriel Raimundo <raimundogabriel18@gmail.com>
2023-11-23 19:49:24 -0300

    endpoints get e put

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Example(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_length=50)
+    Idade = models.IntegerField()
+    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
+
+
+    
+    def __str__(self):
+        return self.field1
+    
